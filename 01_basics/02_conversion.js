6549311d03f19d2a6e1b93048dec58fda888fba6
Ayush470 <99581613+Ayush470@users.noreply.github.com>
2024-02-08 13:23:24 +0000

    modified

---
+++ 01_basics/02_conversion.js
@@ -0,0 +1,8 @@
+let score = "33abc" // if null then converted into 0 you can check other conversion too.
+
+console.log(typeof score)
+console.log(typeof(score))
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber)
